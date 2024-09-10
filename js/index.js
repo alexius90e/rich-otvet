713ea2337b9f095975d41c738dd0db637e5f1dd2
Aliaksei Karzhou <alexius90e@gmail.com>
2024-09-10 20:04:05 +0300

    feat: added conclusions

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.conclusions .swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  navigation: {
+    prevEl: '.conclusions__slider-controls-prev',
+    nextEl: '.conclusions__slider-controls-next',
+  },
+});
